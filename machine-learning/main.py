836535bb0ecab278865c76914bcae94c72b6425d
Jonas Dias <zdias1413@gmail.com>
2024-03-19 17:37:55 -0300

    feat: preparing data for training

--- machine-learning/main.py
+++ machine-learning/main.py
@@ -1,4 +1,9 @@
 import pandas as pd
-from sklearn import tree, neighbors
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('data/NintendoGames.csv')
+
+features = df.drop('title', axis=1)
+targets = df['title']
+
+X_train, X_test, y_train, y_test = train_test_split(features, targets, test_size=0.33, random_state=5)
