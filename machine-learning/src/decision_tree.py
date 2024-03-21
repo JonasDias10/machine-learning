8159356f641ad68c85da521433cf941e1e6042af
Jonas Dias <zdias1413@gmail.com>
2024-03-21 17:11:52 -0300

    feat: add save the decision tree pdf and show the score

--- machine-learning/src/decision_tree.py
+++ machine-learning/src/decision_tree.py
@@ -1,6 +1,13 @@
-from sklearn import tree
+from sklearn.tree import DecisionTreeRegressor, plot_tree
+from matplotlib import pyplot as plt
 
-def decision_tree(X_train, X_test, y_train, y_test):
-      clf = tree.DecisionTreeClassifier()
-      clf = clf.fit(X_train, y_train)
-      return clf.predict(X_test)
+def decision_tree_regressor(X_train, X_test, y_train, y_test, file_name):
+      dtr = DecisionTreeRegressor()
+      dtr.fit(X_train, y_train)
+
+      plot_tree(dtr, filled=True, feature_names=X_train.columns) 
+
+      plt.savefig('outputs/' + file_name, format='pdf')
+      print('Pdf decision tree regressor saved: outputs/' + file_name)
+
+      print('Decision tree regressor score: ', round(dtr.score(X_test, y_test) * 100, 2), '%')
