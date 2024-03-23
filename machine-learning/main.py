c065f4501183ecd0101657c5ffee43195399d931
Jonas Dias <zdias1413@gmail.com>
2024-03-23 00:27:31 -0300

    wip: remove regression models

--- machine-learning/main.py
+++ machine-learning/main.py
@@ -1,29 +1,8 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from src import data_preprocessing, decision_tree as dtr, linear_regression as lr
+from src import data_preprocessing, decision_tree as dt, save_score_histograms as ssh
 
 df = pd.read_csv('data/NintendoGames.csv')
 
-# preprocessing data with meta_score
-df, titles, targets = data_preprocessing.data_preprocessing(df, target_column='meta_score')
-
-# train test split with 33% test size and meta_score as target
-X_train, X_test, y_train, y_test = train_test_split(df, targets, test_size=0.33, random_state=5)
-
-dtr_score = dtr.decision_tree_regressor(X_train, X_test, y_train, y_test)
-lr_score = lr.linear_regression(X_train, X_test, y_train, y_test)
-
-print('Decision tree score with meta_score: ' + str(dtr_score) + '%')
-print('Linear regression score with meta_score: ' + str(lr_score) + '%')
-
-# preprocessing data with user_score
-df, titles, targets = data_preprocessing.data_preprocessing(df, target_column='user_score')
-
-# train test split with 33% test size and user_score as target
-X_train, X_test, y_train, y_test = train_test_split(df, targets, test_size=0.33, random_state=5)
-
-dtr_score = dtr.decision_tree_regressor(X_train, X_test, y_train, y_test)
-lr_score = lr.linear_regression(X_train, X_test, y_train, y_test)
-
-print('Decision tree score with user_score: ' + str(dtr_score) + '%')
-print('Linear regression score with user_score: ' + str(lr_score) + '%')
+# save score histograms
+ssh.save_score_histograms(df)
--- machine-learning/src/decision_tree.py
+++ machine-learning/src/decision_tree.py
@@ -1,15 +1,15 @@
-from sklearn.tree import DecisionTreeRegressor, plot_tree
+from sklearn.tree import DecisionTreeClassifier, plot_tree
 from matplotlib import pyplot as plt
 
-def decision_tree_regressor(X_train, X_test, y_train, y_test):
-      dtr = DecisionTreeRegressor()
-      dtr.fit(X_train, y_train)
+def decision_tree_classifier(X_train, X_test, y_train, y_test):
+      dtc = DecisionTreeClassifier()
+      dtc.fit(X_train, y_train)
 
-      plot_tree(dtr, filled=True, feature_names=X_train.columns) 
+      plot_tree(dtc, filled=True, feature_names=X_train.columns) 
 
-      plt.savefig('outputs/dtr_' + y_train.name + '.pdf', format='pdf')
-      print('Pdf decision tree regressor saved: outputs/' + 'dtr_' + y_train.name + '.pdf')
+      plt.savefig('outputs/dtc_' + y_train.name + '.pdf', format='pdf')
+      print('Pdf decision tree classifier saved: outputs/' + 'dtc_' + y_train.name + '.pdf')
 
-      score = round(dtr.score(X_test, y_test) * 100, 2)
+      score = round(dtc.score(X_test, y_test) * 100, 2)
       
       return score
