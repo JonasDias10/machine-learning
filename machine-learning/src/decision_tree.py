61ed7b64b45080c71d460f3fde9f6b0649c19e41
Jonas Dias <zdias1413@gmail.com>
2024-03-22 23:09:11 -0300

    chore: file name definition and score return

--- machine-learning/src/decision_tree.py
+++ machine-learning/src/decision_tree.py
@@ -1,13 +1,15 @@
 from sklearn.tree import DecisionTreeRegressor, plot_tree
 from matplotlib import pyplot as plt
 
-def decision_tree_regressor(X_train, X_test, y_train, y_test, file_name):
+def decision_tree_regressor(X_train, X_test, y_train, y_test):
       dtr = DecisionTreeRegressor()
       dtr.fit(X_train, y_train)
 
       plot_tree(dtr, filled=True, feature_names=X_train.columns) 
 
-      plt.savefig('outputs/' + file_name, format='pdf')
-      print('Pdf decision tree regressor saved: outputs/' + file_name)
+      plt.savefig('outputs/dtr_' + y_train.name + '.pdf', format='pdf')
+      print('Pdf decision tree regressor saved: outputs/' + 'dtr_' + y_train.name + '.pdf')
 
-      print('Decision tree regressor score: ', round(dtr.score(X_test, y_test) * 100, 2), '%')
+      score = round(dtr.score(X_test, y_test) * 100, 2)
+      
+      return score
