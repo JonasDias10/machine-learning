387baf43b49f4582a6537955ade8929da0812f4b
Jonas Dias <zdias1413@gmail.com>
2024-03-25 12:56:47 -0300

    feat: add a way to save the confusion matrix

--- machine-learning/src/k_neighbors.py
+++ machine-learning/src/k_neighbors.py
@@ -3,13 +3,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
-from sklearn.metrics import f1_score
 from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def k_neighbors(df, targets):
       X_train, X_test, y_train, y_test = train_test_split(df, targets, test_size=0.2, random_state=42)
 
-      knn = KNeighborsClassifier(n_neighbors=2)
+      knn = KNeighborsClassifier()
       knn.fit(X_train, y_train)
 
       y_pred = knn.predict(X_test)
@@ -18,10 +19,22 @@ def k_neighbors(df, targets):
       precision = precision_score(y_test, y_pred) * 100
       recall = recall_score(y_test, y_pred) * 100
       
+      good_game = y_test.value_counts()[1]
+      bad_game = y_test.value_counts()[0]
+
       # save txt with the results
       with open('outputs/k_neighbors_results.txt', 'w') as f:
             f.write('K-Nearest Neighbors Results\n\n')
             f.write('Accuracy: {:.2f}%\n'.format(accuracy))
             f.write('Precision: {:.2f}%\n'.format(precision))
             f.write('Recall: {:.2f}%\n'.format(recall))
-            
+            f.write('Good Game count: {} | Bad Game count: {}\n'.format(good_game, bad_game))
+
+      # save confusion matrix as png
+      labels = ['Bad Game', 'Good Game']
+      cm = confusion_matrix(y_test, y_pred)
+      sns.heatmap(cm, annot=True, cmap='Blues', cbar=False, xticklabels=labels, yticklabels=labels)
+      plt.xlabel('Predicted Labels')
+      plt.ylabel('True Labels')
+      plt.title('K-Nearest Neighbors Confusion Matrix')
+      plt.savefig('outputs/knn_confusion_matrix.png')
