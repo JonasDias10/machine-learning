418dc60a0d4d40614e9216a91d4d38b6a37c5647
Jonas Dias <zdias1413@gmail.com>
2024-03-28 00:14:04 -0300

    feat: add save data for good and bad games

--- machine-learning/src/save_score_histograms.py
+++ machine-learning/src/save_score_histograms.py
@@ -15,9 +15,16 @@ def save_score_histograms(df):
       df['user_score'].hist()
       plt.savefig('outputs/histogram_user_score.png')
 
-      plt.figure(figsize=(12, 6))
-      plt.title('Histogram of best games from Nintendo games')
-      plt.xlabel('1 - Good Game | 0 - Bad Game | Total Games [ {} ]'.format(len(df['best_game'])))
-      plt.ylabel('Frequency of games')
-      df['best_game'].hist()
-      plt.savefig('outputs/histogram_best_games.png')
+      # Save the list of good games with titles, meta scores, and user scores
+      good_games_data = df[df['best_game'] == 1].sort_values(by='meta_score', ascending=False)[['decoded_titles', 'meta_score', 'user_score']]
+      with open('outputs/good_games.txt', 'w') as file:
+            file.write(f"{'Title':85} | Meta Score | User Score\n")
+            for index, row in good_games_data.iterrows():
+                  file.write(f"{row['decoded_titles']:85} | {row['meta_score']:10} | {row['user_score']:10}\n")
+
+      # Save the list of bad games with titles, meta scores, and user scores 
+      bad_games_data = df[df['best_game'] == 0].sort_values(by='meta_score', ascending=False)[['decoded_titles', 'meta_score', 'user_score']]
+      with open('outputs/bad_games.txt', 'w') as file:
+            file.write(f"{'Title':85} | Meta Score | User Score\n")
+            for index, row in bad_games_data.iterrows():
+                  file.write(f"{row['decoded_titles']:85} | {row['meta_score']:10} | {row['user_score']:10}\n")
